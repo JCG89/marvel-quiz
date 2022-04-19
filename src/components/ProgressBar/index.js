49d4019982a39ab28feabcb1606295181bacd06b
Jean Christophe Gomis <christophegomis89@gmail.com>
2022-04-19 13:07:47 +0200

    mise à jour du composant quiz , affichage des niveaux , questions et options

---
+++ src/components/ProgressBar/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ProgressBar = () => {
+  return (
+    <>
+      <div className="percentage">
+        <div className="progressPercent">Question: 1/10</div>
+        <div className="progressPercent">Progession: 10%</div>
+      </div>
+      <div className="progressBar">
+        <div className="progressBarChange" style={{ width: "10%" }}></div>
+      </div>
+    </>
+  );
+};
+export default ProgressBar;
--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -1,11 +1,57 @@
 import React, { Component } from "react";
+import Levels from "../Levels";
+import ProgressBar from "../ProgressBar";
+import { QuizMarvel } from "../QuizMarvel";
 
 class Quiz extends Component {
+  state = {
+    levelNames: ["debutant", "confirme", "expert"],
+    levelQuiz: 0,
+    maxQuestions: 10,
+    storedQuestions: [],
+    question: null,
+    options: [],
+    idQuestion: 0,
+  };
+  loadQuestions = (quizz) => {
+    const fetchedArrQuiz = QuizMarvel[0].quizz[quizz];
+    if (fetchedArrQuiz.length >= this.state.maxQuestions) {
+      const newArrQuiz = fetchedArrQuiz.map(
+        ({ answer, ...keepRest }) => keepRest
+      );
+      this.setState({
+        storedQuestions: newArrQuiz,
+      });
+    } else {
+      console.log("Pas assez de questions");
+    }
+  };
+  componentDidMount() {
+    this.loadQuestions(this.state.levelNames[this.state.levelQuiz]);
+  }
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (this.state.storedQuestions !== prevState.storedQuestions) {
+      this.setState({
+        question: this.state.storedQuestions[this.state.idQuestion].question,
+        options: this.state.storedQuestions[this.state.idQuestion].options,
+      });
+    }
+  }
+
   render() {
     const { pseudo } = this.props.userData;
+
+    const displayOtions = this.state.options.map((option, index) => {
+      return <p className="answerOptions">{option}</p>;
+    });
     return (
       <div>
-        <p>PSEUDO:{pseudo} </p>
+        <Levels />
+        <ProgressBar />
+        <h2>{this.state.question}</h2>
+        {displayOtions}
+
+        <button className="btnSubmit">Suivant</button>
       </div>
     );
   }
