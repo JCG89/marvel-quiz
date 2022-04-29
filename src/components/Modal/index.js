1ac9989748c446ddcef441da75ac995022343f93
Jean Christophe Gomis <christophegomis89@gmail.com>
2022-04-29 13:51:49 +0200

    affichage de la data sur la modal

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -3,7 +3,7 @@ import React from "react";
 const Modal = ({ showModal, children, hideModal }) => {
   return (
     showModal && (
-      <div className="modalBackground" onClick={hideModal}>
+      <div className="modalBackground">
         <div className="modalContainer">{children}</div>
       </div>
     )
--- src/components/QuizOver/index.js
+++ src/components/QuizOver/index.js
@@ -71,6 +71,9 @@ const QuizOver = React.forwardRef((props, ref) => {
     setOpenModal(false);
     setLoading(true);
   };
+  const capitalizeLeter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
   const averageGrade = maxQuestions / 2;
 
   const decision =
@@ -160,10 +163,44 @@ const QuizOver = React.forwardRef((props, ref) => {
         <h2>{characterInfo.data.results[0].name}</h2>
       </div>
       <div className="modalBody">
-        <h4>Titre 2</h4>
+        <div className="comicImage">
+          <img
+            src={
+              characterInfo.data.results[0].thumbnail.path +
+              "." +
+              characterInfo.data.results[0].thumbnail.extension
+            }
+            alt={characterInfo.data.results[0].name}
+          />
+          {characterInfo.attributionText}
+        </div>
+        <div className="comicDetails">
+          <h3>Description</h3>
+          {characterInfo.data.results[0].description ? (
+            <p>{characterInfo.data.results[0].description}</p>
+          ) : (
+            <p>Description indisponible</p>
+          )}
+          <h3>Plus d'infos</h3>
+          {characterInfo.data.results[0].urls &&
+            characterInfo.data.results[0].urls.map((url, index) => {
+              return (
+                <a
+                  key={index}
+                  href={url.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {capitalizeLeter(url.type)}
+                </a>
+              );
+            })}
+        </div>
       </div>
       <div className="modalFooter">
-        <button className="modalBtn">X</button>
+        <button className="modalBtn" onClick={hideModal}>
+          X
+        </button>
       </div>
     </>
   ) : (
